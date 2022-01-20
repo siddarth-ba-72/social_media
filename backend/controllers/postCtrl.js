fa911cc16d53c593d21610d002322d32e06942ad
Siddarth72 <72@siddarth.com>
2022-01-20 19:49:55 +0530

    Delet post

--- backend/controllers/postCtrl.js
+++ backend/controllers/postCtrl.js
@@ -31,6 +31,44 @@ exports.createPost = async (req, res) => {
 	};
 };
 
+exports.deletePost = async (req, res) => {
+	try {
+
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({
+				success: false,
+				message: "Post not found"
+			});
+		}
+
+		if (post.owner.toString() !== req.user._id.toString()) {
+			return res.status(401).json({
+				success: false,
+				message: "You are not authorized to delete this post"
+			});
+		}
+
+		await post.remove();
+
+		const user = await User.findById(req.user._id);
+		const idx = user.posts.indexOf(req.params.id);
+		user.posts.splice(idx, 1);
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Post deleted",
+		});
+
+	} catch (error) {
+		res.status(500).json({
+			status: false,
+			message: error.message
+		});
+	}
+};
+
 exports.likeOrUnlikePost = async (req, res) => {
 	try {
 
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const {
 	createPost,
 	likeOrUnlikePost,
+	deletePost,
 } = require('../controllers/postCtrl.js');
 
 const { authenticatedUser } = require('../middlewares/authMiddleware.js');
 
 router.route("/post/upload").post(authenticatedUser, createPost)
 
-router.route("/post/:id").get(authenticatedUser, likeOrUnlikePost);
+router.route("/post/:id")
+	.get(authenticatedUser, likeOrUnlikePost)
+	.delete(authenticatedUser, deletePost);
 
 module.exports = router;
