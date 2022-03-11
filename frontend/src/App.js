e79c8c30d0c9fe191f141564c17ded5db00b77bb
Siddarth72 <72@siddarth.com>
2022-03-11 16:44:06 +0530

    Header

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Header from './Components/Header/Header';
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+			</div>
+		</Router>
+	);
+}
+
+export default App;
